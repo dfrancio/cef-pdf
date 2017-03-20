4c95742d605bc56a844a5933402e869f3e192b45
Diego Sogari <diego.sogari@gmail.com>
2017-03-20 20:14:39 -0300

    Add library target
    - add cefpdf library target
    - make cef-pdf executable link to the lib
    - add option to build shared version of lib

---
+++ src/lib.h
@@ -0,0 +1,15 @@
+#ifndef LIB_H_
+#define LIB_H_
+
+#include "Job/Manager.h"
+#include "RequestHandler.h"
+
+#include "include/cef_app.h"
+#include "include/cef_client.h"
+#include "include/cef_browser.h"
+
+namespace cefpdf {
+
+} // namespace cefpdf
+
+#endif // LIB_H_
